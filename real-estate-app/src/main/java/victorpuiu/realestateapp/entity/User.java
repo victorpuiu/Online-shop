1b35489afdd77030b3e4bc0209b1809861ca2cee
victorpuiu <puiu.danut@gmail.com>
2023-02-22 12:17:37 +0200

    Update User.

--- real-estate-app/src/main/java/victorpuiu/realestateapp/entity/User.java
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/entity/User.java
@@ -5,13 +5,14 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
+@Entity(name = "user_")
 @Getter
 @Setter
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
     @Column
