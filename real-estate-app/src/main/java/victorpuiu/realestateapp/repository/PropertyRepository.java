fe82c2f3e1e6d8142089e3ce7318c367fae2ba8b
victorpuiu <puiu.danut@gmail.com>
2023-02-22 12:13:40 +0200

    Create PropertyRepository.
    Add PropertyRepository.

--- real-estate-app/src/main/java/victorpuiu/realestateapp/repository/PropertyRepository.java
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/repository/PropertyRepository.java
@@ -3,6 +3,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import victorpuiu.realestateapp.entity.Property;
 
+import java.util.List;
+
 
 /**
  * The database connection with basic CRUD capabilities to the properties table.
@@ -13,5 +15,9 @@ import victorpuiu.realestateapp.entity.Property;
 public interface PropertyRepository extends JpaRepository<Property,Long> {
 
 
+    List<Property> findByPriceLessThanOrEqual(Double max);
 
+    List<Property> findByPriceGreaterThanOrEqual(Double min);
+
+    List<Property> findByPriceBetween(Double min, Double max);
 }
