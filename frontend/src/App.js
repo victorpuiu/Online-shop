8d709fe190b9eeeb458c3f362351f7e2efd116d1
victorpuiu <puiu.danut@gmail.com>
2023-03-07 19:53:28 +0200

    create Home and About routes
    adding Home and About routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,48 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
-
+import Footer from "./components/Footer";
+import {useEffect, useState} from "react";
 
 function App() {
 
-  return (
+    // The effect for the dropdown menu.
+    const [isOpen, setIsOpen] = useState(false);
+    
+    const toggle = () => {
+      setIsOpen(!isOpen);
+    }
+
+    useEffect(() => {
+        const hideMenu = () => {
+            if (window.innerWidth > 768 && isOpen) {
+                setIsOpen(false);
+            }
+        };
+        window.addEventListener("resize", hideMenu);
+
+        return () => {
+            window.removeEventListener("resize", hideMenu);
+        };
+    });
+
+
+    return (
     <div>
       <Routes>
-          <Route path="/" element={ <Home />}/>
 
-          <Route path="/about" element={ <About /> }/>
+          <Route
+              path="/"
+              element={ <Home isOpen={isOpen} toggle={toggle} />}
+          />
+
+          <Route
+              path="/about"
+              element={ <About isOpen={isOpen} toggle={toggle} /> }
+          />
 
       </Routes>
-        <Footer />
+        <Footer/>
 
     </div>
   );
