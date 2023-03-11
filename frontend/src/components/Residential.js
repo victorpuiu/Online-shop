762dd12e3440f75c5b36fa1a11121b5ec95afd9b
victorpuiu <puiu.danut@gmail.com>
2023-03-11 20:03:21 +0200

    update Residential.js

--- frontend/src/components/Residential.js
+++ frontend/src/components/Residential.js
@@ -5,13 +5,13 @@ import beautifulHouse from "../images/beautifulHouse.jpg";
 const Residential = () => {
     const [selectedSubcategory, setSelectedSubcategory] = useState('');
     const [selectedState, setSelectedState] = useState('');
-    const [selectedForSaleBy, setSelectedForSaleBy] = useState('');
+    // const [selectedForSaleBy, setSelectedForSaleBy] = useState('');
     const [selectedPriceRange, setSelectedPriceRange] = useState('');
 
 
     const subcategories = ['House', 'Apartment'];
     const states = ['California', 'New York', 'Florida'];
-    const forSaleByOptions = ['Owner', 'Agent', 'Builder'];
+    // const forSaleByOptions = ['Owner', 'Agent', 'Builder'];
 
     const priceRanges = [
         'Under $58,000.00',
@@ -27,9 +27,9 @@ const Residential = () => {
         setSelectedState(event.target.value);
     };
 
-    const handleForSaleByChange = (event) => {
-        setSelectedForSaleBy(event.target.value);
-    };
+    // const handleForSaleByChange = (event) => {
+    //     setSelectedForSaleBy(event.target.value);
+    // };
 
     const handlePriceFilter = (event) => {
         setSelectedPriceRange(event.target.value);
@@ -68,17 +68,16 @@ const Residential = () => {
                             >
                                 <path
                                     fillRule="even-odd"
-                                    d="M11.293 7.293a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L12 10.414V16a1 1 0 01-2 0v-5.586l-2.293 2.293a1 1 0 01-1 .414-1.414l4-4z"
-                                    clipRule="even-odd"
+                                    d="M10 4a1 1 0 011 1v8.586l3.707-3.707a1 1 0 011.414 1.414l-4.707 4.707a.997.997 0 01-1.414 0L5.879 10.293a1 1 0 011.414-1.414L9 12.586V5a1 1 0 011-1z"
                                 />
                             </svg>
                         </div>
                     </div>
-                    {/*<img src={beautifulHouse} alt="Beautiful House" className="w-1/2 h-48 rounded-lg object-cover" />*/}
                     <div className="relative inline-block w-1/3">
                         <select
                             id="state"
-                            className="w-full appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                            className="w-full appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4
+                            py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
                             value={selectedState}
                             onChange={handleStateChange}
                         >
@@ -97,8 +96,7 @@ const Residential = () => {
                             >
                                 <path
                                     fillRule="even-odd"
-                                    d="M11.293 7.293a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L12 10.414V16a1 1 0 01-2 0v-5.586l-2.293 2.293a1 1 0 01-1 .414-1.414l4-4z"
-                                    clipRule="even-odd"
+                                    d="M10 4a1 1 0 011 1v8.586l3.707-3.707a1 1 0 011.414 1.414l-4.707 4.707a.997.997 0 01-1.414 0L5.879 10.293a1 1 0 011.414-1.414L9 12.586V5a1 1 0 011-1z"
                                 />
                             </svg>
                         </div>
@@ -126,8 +124,7 @@ const Residential = () => {
                             >
                                 <path
                                     fillRule="even-odd"
-                                    d="M10 4a1 1 0 011 1v8.586l3.707-3.707a1 1 0 011.414 1.414l-4.707 4.707a.997.997 0 01-1.414
-                                    0L5.879 10.293a1 1 0 011.414-1.414L9 12.586V5a1 1 0 011-1z"
+                                    d="M10 4a1 1 0 011 1v8.586l3.707-3.707a1 1 0 011.414 1.414l-4.707 4.707a.997.997 0 01-1.414 0L5.879 10.293a1 1 0 011.414-1.414L9 12.586V5a1 1 0 011-1z"
                                     clipRule="even-odd"
                                 />
                             </svg>
@@ -135,34 +132,7 @@ const Residential = () => {
                     </div>
 
 
-                    {/*<div className="relative inline-block w-1/3">*/}
-                    {/*    <select*/}
-                    {/*        id="for-sale-by"*/}
-                    {/*        className="w-full appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"*/}
-                    {/*        value={selectedForSaleBy}*/}
-                    {/*        onChange={handleForSaleByChange}*/}
-                    {/*    >*/}
-                    {/*        <option value="">For sale by</option>*/}
-                    {/*        {forSaleByOptions.map((option) => (*/}
-                    {/*            <option key={option} value={option}>*/}
-                    {/*                {option}*/}
-                    {/*            </option>*/}
-                    {/*        ))}*/}
-                    {/*    </select>*/}
-                    {/*    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">*/}
-                    {/*        <svg*/}
-                    {/*            className="fill-current h-4 w-4"*/}
-                    {/*            xmlns="http://www.w3.org/2000/svg"*/}
-                    {/*            viewBox="0 0 20 20"*/}
-                    {/*        >*/}
-                    {/*            <path*/}
-                    {/*                fillRule="even-odd"*/}
-                    {/*                d="M11.293 7.293a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L12 10.414V16a1 1 0 01-2 0v-5.586l-2.293 2.293a1 1 0 01-1 .414-1.414l4-4z"*/}
-                    {/*                clipRule="even-odd"*/}
-                    {/*            />*/}
-                    {/*        </svg>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
+
                 </div>
             </div>
         </>
