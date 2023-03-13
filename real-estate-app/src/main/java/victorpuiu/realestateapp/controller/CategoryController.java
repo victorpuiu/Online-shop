4b810f4660b884784c9f3b346faf5bd250263fb8
victorpuiu <puiu.danut@gmail.com>
2023-03-13 17:21:27 +0200

    create CategoryController class

---
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/controller/CategoryController.java
@@ -0,0 +1,13 @@
+package victorpuiu.realestateapp.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("markets/{marketId}/categories")
+public class CategoryController {
+
+
+
+}
