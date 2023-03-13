bf220b9223f673d2db082aed687bd0c0f7f897fd
victorpuiu <puiu.danut@gmail.com>
2023-03-13 17:20:26 +0200

    added MarketCategory entity

--- real-estate-app/src/main/java/victorpuiu/realestateapp/entity/MarketCategory.java
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/entity/MarketCategory.java
@@ -1,5 +1,22 @@
 package victorpuiu.realestateapp.entity;
 
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class MarketCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private Market market;
+
+    @OneToMany(mappedBy = "marketCategory")
+    private List<Product> products = new ArrayList<>();
 
 }
