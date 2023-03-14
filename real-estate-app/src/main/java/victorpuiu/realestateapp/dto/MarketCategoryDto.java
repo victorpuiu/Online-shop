b07e466c96a163924ef582608220c161d14f505d
victorpuiu <puiu.danut@gmail.com>
2023-03-14 18:40:28 +0200

    create MarketCategoryDto class

---
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/dto/MarketCategoryDto.java
@@ -0,0 +1,16 @@
+package victorpuiu.realestateapp.dto;
+
+import victorpuiu.realestateapp.entity.Market;
+import victorpuiu.realestateapp.entity.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarketCategoryDto {
+
+    private Market market;
+
+    private List<Product> products = new ArrayList<>();
+
+
+}
