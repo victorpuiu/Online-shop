769492c717cbf24c931ecc22ccb99e7ce5519876
victorpuiu <puiu.danut@gmail.com>
2023-03-14 18:41:22 +0200

    create MarketCategoryService interface

---
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/service/MarketCategoryService.java
@@ -0,0 +1,19 @@
+package victorpuiu.realestateapp.service;
+
+
+import victorpuiu.realestateapp.dto.MarketCategoryDto;
+
+import java.util.List;
+
+public interface MarketCategoryService {
+
+    MarketCategoryDto findById(long id);
+
+    void deleteById(long id);
+
+
+    List<MarketCategoryDto> getCategories();
+
+
+
+}
