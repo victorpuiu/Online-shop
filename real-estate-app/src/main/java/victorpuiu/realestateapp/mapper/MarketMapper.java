052eb47cbcbb96ee733c64e4e38992cae4cccdb1
victorpuiu <puiu.danut@gmail.com>
2023-03-14 18:42:36 +0200

    create MarketMapper interface

---
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/mapper/MarketMapper.java
@@ -0,0 +1,17 @@
+package victorpuiu.realestateapp.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import victorpuiu.realestateapp.dto.MarketDto;
+import victorpuiu.realestateapp.entity.Market;
+
+@Mapper
+public interface MarketMapper {
+    MarketMapper INSTANCE = Mappers.getMapper(MarketMapper.class);
+
+    MarketDto toMarketDto(Market market);
+
+    Market toMarket(MarketDto marketDto);
+
+
+}
