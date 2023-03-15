2101f915a9c019b789060da12eac9e844f9f187c
victorpuiu <puiu.danut@gmail.com>
2023-03-15 19:59:27 +0200

    update

--- real-estate-app/src/main/java/victorpuiu/realestateapp/repository/MarketRepository.java
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/repository/MarketRepository.java
@@ -1,13 +1,12 @@
 package victorpuiu.realestateapp.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import victorpuiu.realestateapp.entity.Market;
 
+
 @Repository
 public interface MarketRepository extends JpaRepository<Market, Long> {
-
 
 
 }
