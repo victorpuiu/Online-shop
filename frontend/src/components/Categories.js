cf2bc134a5684ec790a5ee9379c4234bc2d22485
victorpuiu <puiu.danut@gmail.com>
2023-03-15 19:50:25 +0200

    update Categories.js component

--- frontend/src/components/Categories.js
+++ frontend/src/components/Categories.js
@@ -3,28 +3,41 @@ import {Link} from 'react-router-dom';
 import Navbar from "./Navbar";
 import Content from "./Content";
 
+
 const categories = [
     {
         name: 'Residential',
         // subcategories: ['House', 'Apartment'],
-        image: 'https://picsum.photos/id/10/300/200',
+        image: 'https://fastly.picsum.photos/id/49/1280/792.jpg?hmac=NnUJy0O9-pXHLmY2loqVs2pJmgw9xzuixgYOk4ALCXU',
         link: '/residential'
     },
     {
         name: 'Commercial',
         // subcategories: ['Office Space', 'Retail Space', 'Warehouse', 'Land'],
-        image: 'https://picsum.photos/id/20/300/200',
+        image: 'https://fastly.picsum.photos/id/43/1280/831.jpg?hmac=glK-rQ0ppFClW-lvjk9FqEWKog07XkOxJf6Xg_cU9LI',
         link: '/commercial'
     },
     {
         name: 'Land',
         // subcategories: ['Residential', 'Commercial', 'Farm', 'Ranch'],
-        image: 'https://picsum.photos/id/30/300/200',
+        image: 'https://fastly.picsum.photos/id/46/3264/2448.jpg?hmac=ZHE8nk-Q9uRp4MxgKNvN7V7pYFvA-9BCv99ltY3HBv4',
         link: '/land'
     }
 ];
 
 export const Categories = () => {
+
+    // const [categories, setCategories] = useState([]);
+    //
+    // useEffect(() => {
+    //     loadCategories();
+    // }, []);
+    //
+    // const loadCategories = async () => {
+    //     const result = await axios.get("localhost:8080/markets");
+    //     console.log(result);
+    // };
+
 
     return (
         <>
