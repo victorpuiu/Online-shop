c0cfc57e6c22b1d99b8d2b80b5204df96b02ac4d
victorpuiu <puiu.danut@gmail.com>
2023-03-15 21:23:45 +0200

    refactor ProductServiceDefault class
    refactor saveOrEdit method

--- real-estate-app/src/main/java/victorpuiu/realestateapp/service/ProductServiceDefault.java
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/service/ProductServiceDefault.java
@@ -20,6 +20,8 @@ public class ProductServiceDefault implements ProductService {
     public static final String DEFAULT_PRICE = "0.00";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ProductServiceDefault.class);
+//        LOGGER.info("Saving product: {}", product.getName());
+
 
 
     private final ProductRepository productRepository;
@@ -40,18 +42,34 @@ public class ProductServiceDefault implements ProductService {
     }
 
     @Override
+    public ProductDto saveOrEdit(ProductDto productDto) {
+        return productRepository.findById(productDto.getId() == null ? 0 : productDto.getId())
+                .map((product) -> edit(product, productDto))
+                .orElseGet(() -> save(productDto));
+
+    }
+
+
+    public ProductDto edit(Product product, ProductDto source) {
+        Product toEdit = ProductMapper.INSTANCE.toProduct(source);
+        toEdit.setId(product.getId());
+
+        return ProductMapper.INSTANCE.toProductDto(productRepository.save(toEdit));
+    }
+
+    public ProductDto save(ProductDto productDto) {
+        return ProductMapper.INSTANCE
+                .toProductDto(productRepository.save(ProductMapper.INSTANCE.toProduct(productDto)));
+    }
+
+
+    @Override
     public ProductDto findById(long id) {
        Optional<Product> productOptional = productRepository.findById(id);
        return productOptional.map(ProductMapper.INSTANCE::toProductDto)
                .orElseThrow(() -> new IllegalArgumentException("Id does not exist"));
 
     }
-
-
-    public Product saveProduct(Product product) {
-        return productRepository.save(product);
-    }
-
 
     @Override
     public void deleteById(long id) {
