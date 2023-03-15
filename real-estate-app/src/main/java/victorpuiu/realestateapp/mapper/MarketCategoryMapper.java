611f7c2192f5b6f48f477a9bfb53b25887c25666
victorpuiu <puiu.danut@gmail.com>
2023-03-15 19:54:48 +0200

    added @Mappings annotation

--- real-estate-app/src/main/java/victorpuiu/realestateapp/mapper/MarketCategoryMapper.java
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/mapper/MarketCategoryMapper.java
@@ -1,15 +1,22 @@
 package victorpuiu.realestateapp.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 import victorpuiu.realestateapp.dto.MarketCategoryDto;
 import victorpuiu.realestateapp.entity.MarketCategory;
 
+@Mapper
 public interface MarketCategoryMapper {
 
     MarketCategoryMapper INSTANCE = Mappers.getMapper(MarketCategoryMapper.class);
 
     MarketCategoryDto toMarketCategoryDto(MarketCategory marketCategory);
 
+    @Mappings({
+            @Mapping(target = "market.id", source = "marketId")
+    })
     MarketCategory toMarketCategory(MarketCategoryDto marketCategoryDto);
 
 }
