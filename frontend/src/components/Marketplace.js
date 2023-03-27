2667c4eeaa27d10a0b2fd981de75b50ad6cc944b
victorpuiu <puiu.danut@gmail.com>
2023-03-27 17:38:30 +0300

    create new component Marketplace.js
    added new page Marketplace.js

---
+++ frontend/src/components/Marketplace.js
@@ -0,0 +1,35 @@
+import {Link} from "react-router-dom";
+import React from "react";
+
+const Marketplace = (marketplaceProps) => {
+
+
+    console.log(marketplaceProps);
+    return (
+
+        <>
+            <div className="text-center">
+
+            <li className=" items-center">
+                <Link to={"/marketplaces/" + marketplaceProps.marketPlace.id + "/categories"} className="mr-1">
+                    {marketplaceProps.marketPlace.name}
+                    <div className="text-center">
+                        <h3 className="text-xl font-bold mb-2 underline px-4"></h3>
+                        <div className="relative h-72 w-full overflow-hidden">
+                            <img
+                                src={marketplaceProps.marketPlace.imageUrl}
+                                alt="MarketImage"
+                                className="absolute h-full w-full object-cover"
+                            />
+                        </div>
+                    </div>
+                </Link>
+
+            </li>
+            </div>
+        </>
+    );
+
+}
+
+export default Marketplace;
