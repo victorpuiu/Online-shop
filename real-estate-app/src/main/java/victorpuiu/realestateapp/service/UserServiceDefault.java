047f3a390da0222d6a8db4c2cf1283bb4a6e7bb0
victorpuiu <puiu.danut@gmail.com>
2023-03-30 17:31:56 +0300

    create UserServiceDefault implementation service

---
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/service/UserServiceDefault.java
@@ -0,0 +1,17 @@
+package victorpuiu.realestateapp.service;
+
+import victorpuiu.realestateapp.dto.UserDto;
+import victorpuiu.realestateapp.dto.UserUpdateRequest;
+
+public class UserServiceDefault implements UserService{
+
+    @Override
+    public UserDto getUserById(long id) {
+        return null;
+    }
+
+    @Override
+    public UserDto updateUser(UserUpdateRequest request) {
+        return null;
+    }
+}
