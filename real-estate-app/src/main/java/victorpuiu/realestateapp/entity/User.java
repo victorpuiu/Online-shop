0f59212f8606d0a3cc1d5bc13f0990a4a972a742
victorpuiu <puiu.danut@gmail.com>
2023-03-30 15:39:37 +0300

    added products field for User

--- real-estate-app/src/main/java/victorpuiu/realestateapp/entity/User.java
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/entity/User.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity(name = "user_")
 @Getter
 @Setter
@@ -19,6 +21,9 @@ public class User {
     private String username;
     @Column
     private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Product> products;
 
 
 
