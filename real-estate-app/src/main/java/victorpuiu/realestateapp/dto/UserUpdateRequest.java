acb30bf5e0ce3de9134ea8b9fe38d16640b7cac2
victorpuiu <puiu.danut@gmail.com>
2023-03-30 17:32:21 +0300

    create UserUpdateRequest dto

---
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/dto/UserUpdateRequest.java
@@ -0,0 +1,13 @@
+package victorpuiu.realestateapp.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserUpdateRequest {
+
+    private Long id;
+    private String userName;
+    private String password;
+}
