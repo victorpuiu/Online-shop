5c889b411574ffb5bbda4a5bea8fccf9da0eb4b2
victorpuiu <puiu.danut@gmail.com>
2023-03-30 15:25:31 +0300

    added findByMarketId method

--- real-estate-app/src/main/java/victorpuiu/realestateapp/repository/MarketCategoryRepository.java
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/repository/MarketCategoryRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import victorpuiu.realestateapp.entity.MarketCategory;
 
+import java.util.List;
+
 
 @Repository
 public interface MarketCategoryRepository extends JpaRepository<MarketCategory, Long> {
+    List<MarketCategory> findByMarketId(long marketId);
 }
