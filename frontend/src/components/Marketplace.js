85acddae32d45a2b502dc95178a46a30c96aedfe
victorpuiu <puiu.danut@gmail.com>
2023-03-30 15:29:49 +0300

    refactor

--- frontend/src/components/Marketplace.js
+++ frontend/src/components/Marketplace.js
@@ -4,20 +4,19 @@ import React from "react";
 const Marketplace = (marketplaceProps) => {
 
 
-    console.log(marketplaceProps);
+    // console.log(marketplaceProps);
     return (
 
         <>
             <div className="text-center">
 
-            <li className=" items-center">
                 <Link to={"/marketplaces/" + marketplaceProps.marketPlace.id + "/categories"} className="mr-1">
                     {marketplaceProps.marketPlace.name}
                     <div className="text-center">
                         <h3 className="text-xl font-bold mb-2 underline px-4"></h3>
                         <div className="relative h-72 w-full overflow-hidden">
                             <img
-                                src={marketplaceProps.marketPlace.imageUrl}
+                                src={marketplaceProps.marketPlace.marketImageUrl}
                                 alt="MarketImage"
                                 className="absolute h-full w-full object-cover"
                             />
@@ -25,7 +24,6 @@ const Marketplace = (marketplaceProps) => {
                     </div>
                 </Link>
 
-            </li>
             </div>
         </>
     );
