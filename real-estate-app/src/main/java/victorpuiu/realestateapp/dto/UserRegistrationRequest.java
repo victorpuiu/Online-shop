60311d73f2a26e8911a39fc817969f44b0814736
victorpuiu <puiu.danut@gmail.com>
2023-03-31 13:37:13 +0300

    create UserRegistrationRequest

---
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/dto/UserRegistrationRequest.java
@@ -0,0 +1,13 @@
+package victorpuiu.realestateapp.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegistrationRequest {
+
+    private String userName;
+    private String password;
+
+}
