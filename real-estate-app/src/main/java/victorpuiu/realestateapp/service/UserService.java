d8d696aa863945e4a82577d207df83941a3656ae
victorpuiu <puiu.danut@gmail.com>
2023-03-31 13:36:11 +0300

    update  UserService interface - added createUser and deleteUser methods

--- real-estate-app/src/main/java/victorpuiu/realestateapp/service/UserService.java
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/service/UserService.java
@@ -1,7 +1,9 @@
 package victorpuiu.realestateapp.service;
 
 import org.springframework.stereotype.Service;
+import victorpuiu.realestateapp.dto.UserDeleteRequest;
 import victorpuiu.realestateapp.dto.UserDto;
+import victorpuiu.realestateapp.dto.UserRegistrationRequest;
 import victorpuiu.realestateapp.dto.UserUpdateRequest;
 
 @Service
@@ -9,4 +11,8 @@ public interface UserService {
     UserDto getUserById(long id);
 
     UserDto updateUser(UserUpdateRequest request);
+
+    UserDto createUser(UserRegistrationRequest request);
+
+    void deleteUser(UserDeleteRequest request);
 }
