5a5aa40c6b6356cbda80bb33412fba6273afd56b
victorpuiu <puiu.danut@gmail.com>
2023-03-31 13:38:56 +0300

    create UserAlreadyExistsException class

---
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/exception/UserAlreadyExistsException.java
@@ -0,0 +1,16 @@
+package victorpuiu.realestateapp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class UserAlreadyExistsException extends RuntimeException {
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+
+    public UserAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
