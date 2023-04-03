cd86a97bdf5f9f6f2b6b465441e1b7fb0afdb4d3
victorpuiu <puiu.danut@gmail.com>
2023-04-03 21:23:32 +0300

    deleted

--- frontend/src/images/deleted/Suggest.js
+++ frontend/src/images/deleted/Suggest.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import logoImage from "../images/logo.png";
+import logoImage from "../logo.png";
 import {Link, useNavigate} from "react-router-dom";
 import axios from "axios";
 
