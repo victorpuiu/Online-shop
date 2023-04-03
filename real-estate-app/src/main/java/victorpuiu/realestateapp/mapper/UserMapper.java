92a022ea1756317dc1f1747ea778bf4172c62380
victorpuiu <puiu.danut@gmail.com>
2023-04-03 12:07:10 +0300

    added @Mapper annotation

--- real-estate-app/src/main/java/victorpuiu/realestateapp/mapper/UserMapper.java
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/mapper/UserMapper.java
@@ -1,16 +1,19 @@
 package victorpuiu.realestateapp.mapper;
 
+import org.mapstruct.Mapper;
 import victorpuiu.realestateapp.dto.UserDto;
 import victorpuiu.realestateapp.entity.User;
 
+@Mapper
 public interface UserMapper {
-    public static UserDto toUserDto(User user){
+    static UserDto toUserDto(User user){
         if (user == null){
             return null;
         }
         UserDto userDto = new UserDto();
         userDto.setId(user.getId());
         userDto.setUsername(user.getUsername());
+        userDto.setEmail(user.getEmail());
 
         return userDto;
     }
