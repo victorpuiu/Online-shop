fd9f87e0a0fa95a4b5c322185b9196304f4f4cd7
victorpuiu <puiu.danut@gmail.com>
2023-04-21 23:51:55 +0300

    update

--- real-estate-app/src/main/java/victorpuiu/realestateapp/dto/UserDto.java
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/dto/UserDto.java
@@ -6,8 +6,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserDto {
+
     private Long id;
+
     private String username;
+
     private String email;
 
 //    private String password;
