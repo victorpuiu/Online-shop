bc098f52534eb126a3040f1d31ef9ff8d1742958
victorpuiu <puiu.danut@gmail.com>
2023-04-30 01:57:54 +0300

    refactor

--- real-estate-app/src/main/java/victorpuiu/realestateapp/service/LoginServiceDefault.java
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/service/LoginServiceDefault.java
@@ -24,7 +24,9 @@ public class LoginServiceDefault implements LoginService {
     @Override
     public UserDto login(LoginRequest loginRequest) {
 
-        Optional<User> optionalUser = userRepository.findByEmail(loginRequest.getEmail());
+//        Optional<User> optionalUser = userRepository.findByEmail(loginRequest.getEmail());
+        Optional<User> optionalUser = userRepository.findByUsername(loginRequest.getUsername());
+
         User user = optionalUser.orElseThrow(() -> new IllegalStateException("No user found"));
 
 //        String hashedPassword = hashPassword(loginRequest.getPassword());
