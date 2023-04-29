a4ed72ae23c770874f7d743415b462bde112e885
victorpuiu <puiu.danut@gmail.com>
2023-04-30 01:58:42 +0300

    added home() method for testing

--- real-estate-app/src/main/java/victorpuiu/realestateapp/controller/LoginController.java
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/controller/LoginController.java
@@ -2,6 +2,7 @@ package victorpuiu.realestateapp.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,12 @@ public class LoginController {
     public LoginController(LoginService loginService) {
         this.loginService = loginService;
     }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, page!";
+    }
+
 
     @PostMapping("login")
     public ResponseEntity<UserDto> login(@RequestBody LoginRequest loginRequest){
