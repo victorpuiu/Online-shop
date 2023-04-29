00631e725cc51cf863f2004162e08a313d1af332
victorpuiu <puiu.danut@gmail.com>
2023-04-30 01:55:32 +0300

    added new annotation EnableConfigurationProperties

--- real-estate-app/src/main/java/victorpuiu/realestateapp/RealEstateAppApplication.java
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/RealEstateAppApplication.java
@@ -2,7 +2,10 @@ package victorpuiu.realestateapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import victorpuiu.realestateapp.config.RsaKeyProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class RealEstateAppApplication {
 
