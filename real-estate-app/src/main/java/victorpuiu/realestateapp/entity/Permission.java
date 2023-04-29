a2ee4f3a3a4d8cc9fbab6120189d0072e3810536
victorpuiu <puiu.danut@gmail.com>
2023-04-30 01:57:33 +0300

    refactor PermissionLevel field to Role

--- real-estate-app/src/main/java/victorpuiu/realestateapp/entity/Permission.java
+++ real-estate-app/src/main/java/victorpuiu/realestateapp/entity/Permission.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
-import victorpuiu.realestateapp.model.PermissionLevel;
+import victorpuiu.realestateapp.model.Role;
 
 /**
  * The account resource with specified user details
@@ -18,5 +18,5 @@ public class Permission {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private PermissionLevel permissionLevel;
+    private Role role;
 }
